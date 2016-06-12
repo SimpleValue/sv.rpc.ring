03b08a52b847c45ab90d27322ed2ad6cf050ac36
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-12 14:29:25 +0200

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv.rpc/ring "0.2.0-SNAPSHOT"
+(defproject sv.rpc/ring "0.2.0"
   :description "Provides remote procedure calls via Ring."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
